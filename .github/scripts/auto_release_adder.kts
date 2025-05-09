ab1e13670115d4caa5bc8afb51934c7f52c82085
KYM02 <gmyeongmin@gmail.com>
2025-05-09 21:35:41 +0900

    Release v1.1.10

--- .github/scripts/auto_release_adder.kts
+++ .github/scripts/auto_release_adder.kts
@@ -10,7 +10,7 @@ fun searchReleaseVersion(prBody: String): String {
 
 fun searchReleaseBody(prBody: String): String {
     var bodyRegex = Regex("# 변경 사항\\s*([\\s\\S]*)")
-    return bodyRegex.find(string)?.groups?.get(1)?.value?.replace("\r\n","\\n") ?: NO_BODY_DATA
+    return bodyRegex.find(prBody)?.groups?.get(1)?.value?.replace("\r\n","\\n") ?: NO_BODY_DATA
 }
 
 fun restoreOutput(releaseVersion: String, releaseBody: String) {
